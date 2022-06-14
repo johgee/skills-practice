6a22aed110ee99e37798ea2415d74da2af4fd4ac
Grace Joh <graceeunaejoh@gmail.com>
2022-06-14 15:19:13 -0400

    practice easy method

--- arrays_reduce.rb
+++ arrays_reduce.rb
@@ -1,99 +1,42 @@
 # #  1. Start with an array of numbers and compute the sum of all the numbers.
 # #     For example, [5, 10, 8, 3] becomes 26.
-# numbers = [5, 10, 8, 3]
-# sum = numbers.reduce(0) do |sum, number|
-#   sum + number
-# end
-# p sum
+numbers = [5, 10, 8, 3]
 
 # #  2. Start with an array of strings and combine them all into a single string.
 # #     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
-# sports = ["volleyball", "basketball", "badminton"]
-# word = sports.reduce("") do |word, sport|
-#   word + sport
-# end
-# p word
+sports = ["volleyball", "basketball", "badminton"]
 
 # #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
 # #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
-# items = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
-# total = items.reduce(0) do |total, item|
-#   total + item[:price]
-# end
-# p total
+items = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
 
 # #  4. Start with an array of numbers and compute the the minumum number.
 # #     For example, [5, 10, 8, 3, 9] becomes 3.
-# numbers = [5, 10, 8, 3, 9]
-# minimum = numbers.reduce(numbers[0]) do |min, number|
-#   if number < min
-#     number
-#   else
-#     min
-#   end
-# end
-# p minimum
+numbers = [5, 10, 8, 3, 9]
 
 # #  5. Start with an array of strings and compute the total length of all the strings.
 # #     For example, ["volleyball", "basketball", "badminton"] becomes 29.
-# sports = ["volleyball", "basketball", "badminton"]
-# total = sports.reduce(0) do |sum, sport|
-#   sum + sport.length
-# end
-# p total
+sports = ["volleyball", "basketball", "badminton"]
 
 # #  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
 # #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
-# items = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
-# cheapest = items.reduce(items[0]) do |cheapest, item|
-#   if item[:price] < cheapest[:price]
-#     item
-#   else
-#     cheapest
-#   end
-# end
-# p cheapest
+items = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
 
 #  7. Start with an array of numbers and compute product of all the numbers.
 #     For example, [5, 10, 8, 3] becomes 1200.
 numbers = [5, 10, 8, 3]
-product = numbers.reduce(1) do |product, number|
-  product * number
-end
-
-p product
 
 #  8. Start with an array of strings and combine them all into a single string, separated by dashes.
 #     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
 strings = ["volleyball", "basketball", "badminton"]
-single = strings.reduce("-") do |total, string|
-  total + "#{string}-"
-end
-p single
 
 #  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
 items = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
-shortest = items.reduce(items[0]) do |shortest, item|
-  if item[:name].length < shortest[:name].length
-    item
-  else
-    shortest
-  end
-end
-p shortest
 
 # 10. Start with an array of numbers and compute the maximum number.
 #     For example, [5, 10, 8, 3] becomes 10.
 numbers = [5, 10, 8, 3]
-max = numbers.reduce(numbers[0]) do |max, number|
-  if number > max
-    number
-  else
-    max
-  end
-end
-p max
 
 #sol(using while loop): https://gist.github.com/peterxjang/376c8931a48549889eb3c9bc091e9f43
 #sol(using .each shortcut): https://gist.github.com/peterxjang/379c9945774f51027750c59d6e4395df
--- method.rb
+++ method.rb
@@ -1,21 +1,63 @@
 # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+# puts "enter a word: "
+# word = gets.chomp
+# puts word.upcase
 
 # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+# puts "enter a number: "
+# number = gets.chomp
+# if number > 100
+#   puts "that's a big number!!!"
+# end
 
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+# puts "enter a number: "
+# number = gets.chomp
+# puts "enter another_number: "
+# another_number = gets.chomp
+# p number.to_i + another_number.to_i
 
 # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+# puts "enter a word: "
+# word = gets.chomp
+# puts word.reverse
 
 # 5. Write a program that asks the user to enter a number, then prints the number times 10.
+# puts "enter a number: "
+# number = gets.chomp
+# puts number.to_i * 10
 
 # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+# puts "enter a word: "
+# word = gets.chomp
+# puts "enter another_word: "
+# another_word = gets.chomp
+# puts "#{word.upcase} #{another_word.upcase}"
 
 # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+# puts "enter a word: "
+# word = gets.chomp
+# puts word.length
 
 # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+# puts "enter a number: "
+# number = gets.chomp
+# if number.to_i < 0
+#   puts "that's a negative number!"
+# end
 
 # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+# puts "enter a number: "
+# number = gets.chomp
+# puts "enter another_number: "
+# another_number = gets.chomp
+# puts number.to_i * another_number.to_i
 
 # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+puts "enter a word: "
+word = gets.chomp
+if word.length > 5
+  puts "that's a long word!"
+end
 
 # sol: https://gist.github.com/peterxjang/1539a3ad79728ba4fb68dd8d07279c29
