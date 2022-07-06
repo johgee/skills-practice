b57b0c9d88c313be8e971a7f3168b50b33776de7
Grace Joh <graceeunaejoh@gmail.com>
2022-07-05 23:11:16 -0400

    still the wrong answer

--- mockinterview.rb
+++ mockinterview.rb
@@ -34,52 +34,51 @@
 # return create a most_common_letter = "
 # end
 
-# def common_letter(string)
-#   most_common_letter = ""
-#   current_char = string[0]
-#   most_common_letter_count = 0
-#   letter_count = 0
-#   index = 0
-#   while index < string.length
-#     if current_char == string[index]
-#       letter_count = letter_count + 1
-#       if most_common_letter_count < letter_count
-#         most_common_letter_count = letter_count
-#         most_common_letter = current_char
-#       end
-#     end
-#     index += 1
-#   end
-#   most_common_letter
-# end
-
-# p common_letter("peter piper picked a peck of pickled peppers")
-
-# count how many there are in a hash
-{ "p" => 9, "e" => 8, "t" => 1, "r" => 3, "i" => 3, " " => 7, "c" => 3, "d" => 2, "a" => 1, "o" => 1, "f" => 1, "l" => 1, "s" => 1 }
-
-def frequent_letter(string)
-  letter_counts = {}
+def common_letter(string)
+  most_common_letter = ""
+  most_common_letter_count = 0
+  letter_count = 0
   index = 0
-  most_frequent_letter = ""
-  most_frequent_count = 0
-
+  current_char = string[index]
   while index < string.length
-    if letter_counts[string[index]]
-      letter_counts[string[index]] += 1
-    else
-      letter_counts[string[index]] = 1
+    if current_char == most_common_letter
+      most_common_letter = current_char
+      if letter_count == most_common_letter_count
+        most_common_letter_count = letter_count
+      end
+      index += 1
     end
-
-    if letter_counts[string[index]] > most_frequent_count
-      most_frequent_count = letter_counts[string[index]]
-      most_frequent_letter = string[index]
-    end
-
-    index += 1
   end
-
-  return most_frequent_letter
+  most_common_letter
 end
 
-p frequent_letter("peter piper picked a peck of pickled peppers")
+p common_letter("peter piper picked a peck of pickled peppers")
+
+# count how many there are in a hash
+# { "p" => 9, "e" => 8, "t" => 1, "r" => 3, "i" => 3, " " => 7, "c" => 3, "d" => 2, "a" => 1, "o" => 1, "f" => 1, "l" => 1, "s" => 1 }
+
+# def frequent_letter(string)
+#   letter_counts = {}
+#   index = 0
+#   most_frequent_letter = ""
+#   most_frequent_count = 0
+
+#   while index < string.length
+#     if letter_counts[string[index]]
+#       letter_counts[string[index]] += 1
+#     else
+#       letter_counts[string[index]] = 1
+#     end
+
+#     if letter_counts[string[index]] > most_frequent_count
+#       most_frequent_count = letter_counts[string[index]]
+#       most_frequent_letter = string[index]
+#     end
+
+#     index += 1
+#   end
+
+#   return most_frequent_letter
+# end
+
+# p frequent_letter("peter piper picked a peck of pickled peppers")
